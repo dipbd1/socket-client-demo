8f5261dd8b836880bb3afafd7c4310cc7f21175b
Dip Chowdhury <dipbd1@gmail.com>
2020-10-12 11:03:57 +0600

    Fixing issue

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var path = require('path');
-var enforce = require('express-sslify');
 var serveStatic = require('serve-static');
 
 const app = express();
 
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
 app.listen(port);
